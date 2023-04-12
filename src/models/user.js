1347af23db9306927ca2a1e6b29453d212f55640
kuromika <kuromika.dev@gmail.com>
2023-04-12 16:27:59 -0500

    Make username unique

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   hash: { type: String, required: true },
   salt: { type: String, required: true },
-  isAdmin: { type: Boolean, required: true },
+  isAdmin: { type: Boolean, required: true, default: false },
 });
 
 module.exports = mongoose.model("User", UserSchema);
