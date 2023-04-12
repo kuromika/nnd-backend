eba8935a711cb9067541637ad61c063d90863f12
kuromika <kuromika.dev@gmail.com>
2023-04-12 14:32:49 -0500

    Create user router

---
+++ src/routes/userRouter.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const userController = require("../controllers/userController");
+
+router.get("/", userController.getUsers);
+// router.post("/", userController.createUser);
+// router.put("/:id", userController.updateUser);
+// router.delete("/:id", userController.deleteUser);
+
+module.exports = router;
