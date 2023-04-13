6fa0cb00de2176b383f3c78e2efd9fdb36c6070b
kuromika <kuromika.dev@gmail.com>
2023-04-13 10:44:49 -0500

    Add routes to get & create a single post

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const postController = require("../controllers/postController");
 
 router.get("/", postController.getPosts);
-// router.post('/', postController.createPost);
+router.post("/", postController.createPost);
+router.get("/:id", postController.getPost);
 // router.put('/:id', postController.updatePost);
 // router.delete('/:id', postController.deletePost);
 
