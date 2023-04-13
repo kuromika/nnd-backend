9b7454feab7216d8226f46a77a3f4919026ebd52
kuromika <kuromika.dev@gmail.com>
2023-04-12 21:25:10 -0500

    Replace verify with verifyJwt

--- src/authentication/strategies/jwt.js
+++ src/authentication/strategies/jwt.js
@@ -1,8 +1,8 @@
 const { Strategy, ExtractJwt } = require("passport-jwt");
-const verify = require("../verify");
+const { verifyJwt } = require("../verify");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = process.env.SECRET;
 
-module.exports = new Strategy(opts, verify);
+module.exports = new Strategy(opts, verifyJwt);
