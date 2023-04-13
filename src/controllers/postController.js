163573cf611cf74f7dd27b731e6f4038523abbdf
kuromika <kuromika.dev@gmail.com>
2023-04-13 10:45:23 -0500

    Add controllers to post and get a single post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const { isAdmin } = require("../middleware/authentication");
 
 const getPosts = async (req, res, next) => {
   try {
@@ -9,4 +10,30 @@ const getPosts = async (req, res, next) => {
   }
 };
 
-module.exports = { getPosts };
+const createPost = [
+  isAdmin,
+  async (req, res, next) => {
+    const newPost = new Post({
+      content: req.body.content,
+      user: req.user.id,
+      isPublished: req.body.isPublished,
+    });
+    try {
+      await newPost.save();
+      return res.json(newPost);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
+
+const getPost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    return res.json(post);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = { getPosts, createPost, getPost };
