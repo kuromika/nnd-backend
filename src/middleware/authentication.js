94e147521e589e2c1c56b632c6d04e3a5f50639f
kuromika <kuromika.dev@gmail.com>
2023-04-13 11:36:11 -0500

    Change status from 401 to 403

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -7,7 +7,7 @@ const isAdmin = [
   (req, res, next) => {
     if (!req.user.isAdmin) {
       const error = new Error("Only admins can use this resource");
-      error.status = 401;
+      error.status = 403;
       return next(error);
     }
     return next();
