c188a065c248501b73fd56cd4788a781399a30b9
kuromika <kuromika.dev@gmail.com>
2023-04-13 11:02:00 -0500

    Add PUT route to update a post

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -4,7 +4,7 @@ const postController = require("../controllers/postController");
 router.get("/", postController.getPosts);
 router.post("/", postController.createPost);
 router.get("/:id", postController.getPost);
-// router.put('/:id', postController.updatePost);
+router.put("/:id", postController.updatePost);
 // router.delete('/:id', postController.deletePost);
 
 module.exports = router;
