1101a46b289fa94c9d3a63035e76002f76cb0ce8
kuromika <kuromika.dev@gmail.com>
2023-04-13 11:36:42 -0500

    Add expiration to jwt

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -6,12 +6,17 @@ router.post(
   "/login",
   passport.authenticate("local", { session: false }),
   (req, res, next) => {
-    jwt.sign({ user: req.user }, process.env.SECRET, (err, token) => {
-      if (err) {
-        return next(err);
+    jwt.sign(
+      { user: req.user },
+      process.env.SECRET,
+      { expiresIn: "14d" },
+      (err, token) => {
+        if (err) {
+          return next(err);
+        }
+        return res.json({ token });
       }
-      return res.json({ token });
-    });
+    );
   }
 );
 
