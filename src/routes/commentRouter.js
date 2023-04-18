313b6e4a6c6e01aee1c448c11898d14c8e1e6d34
kuromika <kuromika.dev@gmail.com>
2023-04-18 10:49:17 -0500

    Add DELETE route for comments

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const commentController = require("../controllers/commentController");
 
 router.put("/:id", commentController.updatePostComment);
+router.delete("/:id", commentController.deletePostComment);
 
 module.exports = router;
