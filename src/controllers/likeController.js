113fb5f3b9bb1b3f466676919a94ab4d66b5deba
kuromika <kuromika.dev@gmail.com>
2023-04-22 10:44:05 -0500

    Add controller to create likes

--- src/controllers/likeController.js
+++ src/controllers/likeController.js
@@ -1,4 +1,5 @@
 const Like = require("../models/like");
+const { isAuth } = require("../middleware/authentication");
 
 const getLikes = async (req, res, next) => {
   try {
@@ -18,4 +19,28 @@ const getPostLikes = async (req, res, next) => {
   }
 };
 
-module.exports = { getLikes, getPostLikes };
+const createLike = (model) => [
+  isAuth,
+  async (req, res, next) => {
+    try {
+      const like = {
+        doc: req.params.id,
+        docModel: model,
+        user: req.user.id,
+      };
+      const alreadyLiked = await Like.find(like);
+      if (alreadyLiked.length) {
+        const error = new Error("You can only like something once");
+        error.status = 403;
+        return next(error);
+      }
+      const newLike = new Like(like);
+      const savedLike = await newLike.save();
+      return res.json(savedLike);
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
+
+module.exports = { getLikes, getPostLikes, createLike };
