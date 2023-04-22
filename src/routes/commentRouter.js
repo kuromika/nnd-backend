14903628776b04bf892b21e3cb0240f2a895e17a
kuromika <kuromika.dev@gmail.com>
2023-04-22 10:50:58 -0500

    Add get, post likes routes for comments

--- src/routes/commentRouter.js
+++ src/routes/commentRouter.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
+const likeController = require("../controllers/likeController");
 const commentController = require("../controllers/commentController");
 
 router.get("/", commentController.getComments);
 router.get("/:id", commentController.getComment);
 router.put("/:id", commentController.updatePostComment);
 router.delete("/:id", commentController.deletePostComment);
+
+router.get("/:id/likes", likeController.getDocLikes("Comment"));
+router.post("/:id/likes", likeController.createLike("Comment"));
 
 module.exports = router;
