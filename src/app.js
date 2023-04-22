ff467edc910923fa75988d0fe6ac55330f135085
kuromika <kuromika.dev@gmail.com>
2023-04-22 11:17:57 -0500

    Remove public dir

--- src/app.js
+++ src/app.js
@@ -31,7 +31,6 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 passport.use(jwtStrategy);
 passport.use(localStrategy);
