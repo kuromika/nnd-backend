c9aa50323e9225e70a6ab9815cc95d35d4517c20
kuromika <kuromika.dev@gmail.com>
2023-05-02 20:14:16 -0500

    Change success status

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ app.use(
       "https://natsu-no-daisankaku-a.vercel.app/*",
       "https://natsu-no-daisankaku.vercel.app/*",
     ],
+    optionsSuccessStatus: 200,
   })
 );
 app.use(express.json());
